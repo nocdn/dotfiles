38a4d101f296eca6896e9f307f769341338d4839
Bartek <bartek.bak@protonmail.com>
2024-06-18 17:47:22 +0100

    Added a better setup script, now using zsh

--- remote-setup/setup.sh
+++ remote-setup/setup.sh
@@ -1,21 +1,29 @@
-ln -s /mnt/c/Users/akbb0 ./winhome
-ln -s /mnt/c/Users/akbb0/curseforge/minecraft/Instances ./mcinstances
-
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install -y wget curl zip neofetch python3 pgp gawk make
+sudo apt install -y nano wget curl zip neofetch python3 pgp gawk make fzf git
 
-rm -f .bashrc
-curl -O "https://raw.githubusercontent.com/Kayetic/dotfiles/master/.bashrc?token=GHSAT0AAAAAACTPZHLXG6SQFI3MADZGHEBCZTQU3YQ"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-syntax-highlighting/Raspbian_9.0/ /' | sudo tee /etc/apt/sources.list.d/shells:zsh-users:zsh-syntax-highlighting.list
+curl -fsSL https://download.opensuse.org/repositories/shells:zsh-users:zsh-syntax-highlighting/Raspbian_9.0/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_zsh-users_zsh-syntax-highlighting.gpg > /dev/null
+sudo apt update
+sudo apt install zsh-syntax-highlighting
 
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
 
 add_to_path() {
   # Get the current user's home directory
   USER_HOME=$(eval echo ~"$USER")
   LOCAL_BIN_PATH="$USER_HOME/.local/bin"
-  
+
   # Detect the active shell
   CURRENT_SHELL=$(basename "$SHELL")
 
@@ -52,3 +60,11 @@ add_to_path() {
 
 # Call the function
 add_to_path
+
+git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
+cd zsh-syntax-highlighting/themes/
+cp -v catppuccin_mocha-zsh-syntax-highlighting.zsh ~/.zsh/
+
+echo "source ~/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
