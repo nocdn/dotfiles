60ccc535ee6711db78a18ae42287687bb8bffaaf
Bartek <bartek.bak@protonmail.com>
2024-08-14 02:19:51 +0100

    Fixed to work with sftp and pebblehost new portal change

--- download-server-files.sh
+++ download-server-files.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-# script to download the world/ folder from pebblehost ftp server to backup the server world
-
-# Set FTP credentials and the target directory
-FTP_SERVER="de11.pebblehost.com"
-FTP_USERNAME="bartek.bak@protonmail.com.809868"
-FTP_PASSWORD="Our_Server"
+# Set SFTP credentials and the target directory
+SFTP_SERVER="de11.pebblehost.com"
+SFTP_PORT="2222"
+SFTP_USERNAME="bartek.bak@protonmail.com.42bf55b3"
+SFTP_PASSWORD="Our_Server"
 REMOTE_DIR="world/"
 LOCAL_DIR="."
 
-# Use wget to download the entire directory
-wget -m --user="$FTP_USERNAME" --password="$FTP_PASSWORD" ftp://$FTP_SERVER/$REMOTE_DIR -P $LOCAL_DIR
-
+# Create a dated directory name
 DATE=$(date +"%d-%m-%Y-%H-%M")
 DATED_LOCAL_DIR="server-backup-$DATE"
 
+# Create the dated directory
+mkdir -p "$DATED_LOCAL_DIR"
 
-mv de11.pebblehost.com/ "$DATED_LOCAL_DIR"/
+# Use lftp to download the entire directory
+lftp -u $SFTP_USERNAME,$SFTP_PASSWORD sftp://$SFTP_SERVER:$SFTP_PORT -e "mirror $REMOTE_DIR $DATED_LOCAL_DIR; quit"
 
-zip -r "$DATED_LOCAL_DIR".zip "$DATED_LOCAL_DIR"/
-rm -rf "$DATED_LOCAL_DIR"/
+# Zip the dated directory
+zip -r "$DATED_LOCAL_DIR.zip" "$DATED_LOCAL_DIR"
+
+sleep 2
+
+echo "removing $DATED_LOCAL_DIR"
+
+# Remove the unzipped dated directory
+rm -rf "$DATED_LOCAL_DIR"
