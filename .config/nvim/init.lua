8bc02ce50ab1856f871f734862979b4dab9d10e5
Bartek <bartek.bak@protonmail.com>
2024-08-27 19:29:39 +0100

    Made it so plugin updates do not show on every launch of nvim, and neo-tree panel always shows hidden files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,17 +17,14 @@ vim.cmd("set shiftwidth=2")
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({"git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath})
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({{"Failed to clone lazy.nvim:\n", "ErrorMsg"}, {out, "WarningMsg"},
+                           {"\nPress any key to exit..."}}, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -39,34 +36,49 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
-      'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+    spec = {{
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000
+    }, {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        dependencies = {'nvim-lua/plenary.nvim'}
+    }, {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    }, {"ThePrimeagen/vim-be-good"}, {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = {
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    show_hidden_count = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = true,
+                    hide_by_name = {
+                        -- '.git',
+                        -- '.DS_Store',
+                        -- 'thumbs.db',
+                    },
+                    never_show = {}
+                }
+            }
+        },
+        branch = "v3.x",
+        dependencies = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+                        "MunifTanjim/nui.nvim" -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    }, {"williamboman/mason.nvim"}, {"supermaven-inc/supermaven-nvim"}},
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    install = {
+        colorscheme = {"habamax"}
     },
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-    { "ThePrimeagen/vim-be-good" },
-    {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-      }
-    },
-    {
-      "williamboman/mason.nvim"
-    },
-    { "supermaven-inc/supermaven-nvim" }
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+    -- automatically check for plugin updates
+    checker = {
+        enabled = true
+    }
 })
 
 -- catppuccin color scheme
@@ -81,14 +93,18 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 local configs = require("nvim-treesitter.configs")
 configs.setup({
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-  sync_install = false,
-  highlight = { enable = true },
-  indent = { enable = true },  
+    ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html"},
+    sync_install = false,
+    highlight = {
+        enable = true
+    },
+    indent = {
+        enable = true
+    }
 })
 
 config = function()
-  require("supermaven-nvim").setup({})
+    require("supermaven-nvim").setup({})
 end
 
 vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>')
